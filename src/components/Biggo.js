77bd4a19b1c67e159d05e6af0bfbe3f2aa67cd8d
Cory Reed <swashcap@gmail.com>
2017-06-09 00:09:43 -0700

    Add styles to `Biggo` component.

--- src/components/Biggo.js
+++ src/components/Biggo.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
+  Animated,
+  StatusBar,
   StyleSheet,
   Text,
   TouchableWithoutFeedback,
@@ -10,11 +12,21 @@ import {
 
 const styles = StyleSheet.create({
   backButton: {
-    height: 60,
+    alignItems: 'center',
+    height: '100%',
+    justifyContent: 'center',
     width: 60,
+    zIndex: 2,
   },
-  body: {},
+  backButtonText: {
+    color: 'white',
+    fontFamily: 'Copperplate',
+  },
+  bottom: {
+    backgroundColor: 'indigo',
+  },
   container: {
+    backgroundColor: 'indigo',
     flex: 1,
     flexDirection: 'column',
   },
@@ -22,28 +34,87 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
   },
-  longDescription: {
-    backgroundColor: 'red',
-    height: 200,
+  name: {
+    color: 'white',
+    flexShrink: 2,
+    fontFamily: 'Times New Roman',
+    fontSize: 18,
+    fontWeight: '700',
+    height: 18,
+    lineHeight: 18,
+    marginTop: 6,
+    paddingLeft: 10,
+    paddingRight: 60,
   },
-  name: {},
-  price: {},
-  shortDescription: {},
-  stock: {},
+  nameOutOfStock: {
+    color: 'darksalmon',
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+  },
+  price: {
+    backgroundColor: 'hotpink',
+    bottom: -10,
+    color: 'lemonchiffon',
+    fontFamily: 'Chalkboard SE',
+    fontSize: 24,
+    fontWeight: '700',
+    letterSpacing: -1,
+    lineHeight: 24,
+    paddingTop: 6,
+    paddingRight: 6,
+    paddingLeft: 6,
+    position: 'absolute',
+    right: 5,
+    transform: [{ rotate: '5deg' }],
+  },
+  stock: {
+    backgroundColor: 'transparent',
+    bottom: 10,
+    color: 'red',
+    fontFamily: 'Times New Roman',
+    fontSize: 22,
+    fontStyle: 'italic',
+    fontWeight: '700',
+    left: 0,
+    lineHeight: 22,
+    position: 'absolute',
+    textAlign: 'center',
+    width: '100%',
+  },
   top: {
-    backgroundColor: 'lime',
+    flexDirection: 'row',
+    paddingTop: 22,
+    position: 'relative',
+    width: '100%',
+    zIndex: 2,
   },
 });
-
 
 export default class Biggo extends Component {
 
   // TODO: Better way to do this? Place in stylesheet?
-  static getWebViewHTML({ productImage, longDescription }) {
+  // TODO: Use WKWebView? https://github.com/facebook/react-native/issues/321
+  static getWebViewHTML({
+    longDescription,
+    productImage,
+    productName,
+    reviewCount,
+    reviewRating,
+    shortDescription,
+  }) {
     return `<!doctype html>
 <html>
   <head>
     <style>
+      html {
+        background: linear-gradient(hotpink, indigo);
+        color: white;
+        font-family: 'Chalkboard SE';
+        line-height: 1.25;
+      }
+      * {
+        box-sizing: border-box;
+      }
       body {
         margin: 0
       }
@@ -51,25 +122,144 @@ export default class Biggo extends Component {
         height: auto;
         width: 100%;
       }
+      .name {
+        background: indigo;
+        font-family: 'Times New Roman';
+        line-height: 1;
+        margin: 0;
+        padding: 1rem;
+        text-align: center;
+      }
+      .description {
+        font-size: 125%;
+        font-weight: 400;
+        line-height: 1.1;
+        margin: 0 0 1rem;
+      }
+      .rating {
+        background: indigo;
+        color: pink;
+        display: flex;
+        justify-content: center;
+        padding: .25rem 1rem 1rem;
+        text-align: center;
+      }
+      .rating-stars {
+        font-size: 120%;
+        letter-spacing: .1em;
+        position: relative;
+        overflow: hidden;
+      }
+      .rating-stars:after {
+        background: indigo;
+        content: '☆☆☆☆☆';
+        direction: rtl;
+        display: block;
+        height: 100%;
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: ${reviewRating * 20}%;
+      }
+      .rating-count {
+        padding-left: .5rem;
+      }
+      .content {
+        padding: 1rem;
+      }
     </style>
   </head>
   <body>
+    <h1 class="name">${productName}</h1>
+    <div class="rating">
+      <div class="rating-stars">★★★★★</div>
+      <div class="rating-count">
+        ${reviewCount + (reviewCount === 1 ? ' review' : ' reviews')}
+      </div>
+    </div>
     <img src="${productImage}" />
-    ${longDescription}
+    <div class="content">
+      <h2 class="description">${shortDescription}</h2>
+      ${longDescription}
+    </div>
+    <script>
+      (function () {
+        var headerHeight = document.querySelector('h1').offsetHeight;
+        var lastScroll = 0;
+        var ticking = false;
+
+        window.addEventListener('scroll', function onScroll(event) {
+          lastScroll = window.scrollY;
+          if (!ticking) {
+            window.requestAnimationFrame(function () {
+              postMessage(
+                lastScroll > 0.95 * headerHeight ? 'scrolled' : 'not-scrolled'
+              );
+              ticking = false;
+            });
+          }
+          ticking = true;
+        });
+      })();
+    </script>
   </body>
 </html>`;
   }
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      isTopAnimating: false,
+      topCollapse: new Animated.Value(0),
+    };
+
+    this.animateTop = this.animateTop.bind(this);
+    this.handleWebViewMessage = this.handleWebViewMessage.bind(this);
     this.renderBottom = this.renderBottom.bind(this);
     this.renderTop = this.renderTop.bind(this);
+  }
+
+  /**
+   * Handle `postMessage` from WebView.
+   *
+   * {@link https://facebook.github.io/react-native/docs/webview.html#onmessage}
+   *
+   * @param {Object} event
+   */
+  handleWebViewMessage(event) {
+    const { nativeEvent: { data } } = event;
+    const { isTopAnimating, topCollapse } = this.state;
+
+    if (!isTopAnimating) {
+      if (data === 'scrolled' && topCollapse !== 1) {
+        this.animateTop(1);
+      } else if (data === 'not-scrolled' && topCollapse !== 0) {
+        this.animateTop(0);
+      }
+    }
+  }
+
+  animateTop(toValue) {
+    this.setState({
+      isTopAnimated: true,
+      topCollapse: this.state.topCollapse,
+    });
+
+    Animated.timing(this.state.topCollapse, {
+      duration: 400,
+      toValue,
+    }).start(() => this.setState({
+      isTopAnimated: false,
+      topCollapse: this.state.topCollapse,
+    }));
   }
 
   renderBottom() {
     const {
       longDescription,
       productImage,
+      productName,
       reviewCount,
       reviewRating,
       shortDescription,
@@ -77,47 +267,72 @@ export default class Biggo extends Component {
     const html = Biggo.getWebViewHTML({
       longDescription,
       productImage,
+      productName,
       reviewCount,
       reviewRating,
       shortDescription,
     });
 
+    /* eslint-disable react/jsx-boolean-value */
     return (
       <WebView
-        bounces={false}
+        bounces={true}
         decelerationRate={'normal'}
         javaScriptEnabled={false}
+        onMessage={this.handleWebViewMessage}
         scalesPageToFit={false}
-        scrollEnabled={false}
+        scrollEnabled={true}
         source={{ html }}
-        style={styles.longDescription}
+        style={styles.bottom}
       />
     );
+    /* eslint-enable react/jsx-boolean-value */
   }
 
   renderTop() {
     const { inStock, onBackPress, productName, price } = this.props;
+    const { topCollapse } = this.state;
     const stock = !inStock ?
       <Text style={styles.stock}>Out of Stock</Text> :
       undefined;
 
+    const topStyle = StyleSheet.flatten([styles.top, {
+      height: topCollapse.interpolate({
+        inputRange: [0, 1],
+        outputRange: [75, 55],
+      }),
+    }]);
+    const nameStyle = StyleSheet.flatten([
+      styles.name,
+      (!inStock ? styles.nameOutOfStock : undefined),
+      {
+        opacity: topCollapse,
+      },
+    ]);
+
     return (
-      <View style={styles.top}>
+      <Animated.View style={topStyle}>
         <TouchableWithoutFeedback onPress={onBackPress}>
           <View style={styles.backButton}>
-            <Text>←</Text>
+            <Text style={styles.backButtonText}>&lt; Back</Text>
           </View>
         </TouchableWithoutFeedback>
-        <Text style={styles.name}>{productName}</Text>
+        <Animated.Text style={nameStyle}>
+          {productName}
+        </Animated.Text>
         <Text style={styles.price}>{price}</Text>
         {stock}
-      </View>
+      </Animated.View>
     );
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar
+          backgroundColor="indigo"
+          barStyle="light-content"
+        />
         {this.renderTop()}
         {this.renderBottom()}
       </View>
