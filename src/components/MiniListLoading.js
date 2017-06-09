39165058addaabc8a8b2ee2af2648803d089b397
Cory Reed <swashcap@gmail.com>
2017-06-08 19:31:16 -0700

    Add styles to `MiniListLoading` component.

--- src/components/MiniListLoading.js
+++ src/components/MiniListLoading.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import loadingAnimation from '../images/loading-animation-2.gif';
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    display: 'flex',
+    flex: 1,
+    paddingBottom: 20,
+    paddingTop: 20,
+  },
+  image: {
+    height: 50,
+    marginBottom: 10,
+    width: 80,
   },
   text: {
+    fontFamily: 'Courier New',
+    fontSize: 16,
+    fontWeight: '700',
+    textAlign: 'center',
   },
 });
 
@@ -12,6 +28,10 @@ export default class MiniListLoading extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image
+          source={loadingAnimation}
+          style={styles.image}
+        />
         <Text style={styles.text}>Loading...</Text>
       </View>
     );
