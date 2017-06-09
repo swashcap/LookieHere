a22b4def4eb86fd810b91ada5489bbc6e3c15071
Cory Reed <swashcap@gmail.com>
2017-06-08 19:40:57 -0700

    Add basic styles to `MiniList` and `Mini` components.

--- src/components/Mini.js
+++ src/components/Mini.js
@@ -10,16 +10,57 @@ import {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
   },
   image: {
     height: 100,
     width: 100,
   },
   name: {
+    backgroundColor: 'transparent',
+    fontFamily: 'Times New Roman',
+    fontSize: 24,
+    fontWeight: '700',
+    lineHeight: 24,
+    marginBottom: 5,
+  },
+  nameOutOfStock: {
+    color: 'salmon',
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
   },
   price: {
+    backgroundColor: 'hotpink',
+    color: 'lemonchiffon',
+    fontFamily: 'Chalkboard SE',
+    fontSize: 20,
+    fontWeight: '700',
+    letterSpacing: -1,
+    lineHeight: 20,
+    paddingBottom: 4,
+    paddingLeft: 6,
+    paddingRight: 6,
+    paddingTop: 6,
+    position: 'absolute',
+    right: 10,
+    textAlign: 'center',
+    top: 10,
+    transform: [{ rotate: '5deg' }],
   },
   stock: {
+    backgroundColor: 'transparent',
+    color: 'red',
+    fontFamily: 'Times New Roman',
+    fontSize: 28,
+    fontStyle: 'italic',
+    fontWeight: '700',
+    left: 38,
+    lineHeight: 28,
+    position: 'absolute',
+    top: 35,
+  },
+  top: {
+    position: 'relative',
   },
 });
 
@@ -36,13 +77,16 @@ export default class Mini extends Component {
     const stock = !inStock ?
       <Text style={styles.stock}>Out of Stock</Text> :
       undefined;
+    const nameStyles = !inStock ?
+      StyleSheet.flatten([styles.name, styles.nameOutOfStock]) :
+      styles.name;
 
     return (
       <TouchableOpacity onPress={onPress}>
         <View style={styles.container}>
-          {stock}
-          <Text style={styles.name}>{productName}</Text>
+          <Text style={nameStyles}>{productName}</Text>
           <Text style={styles.price}>{price}</Text>
+          {stock}
           <Image
             source={{ uri: productImage }}
             style={styles.image}
--- src/components/MiniList.js
+++ src/components/MiniList.js
@@ -1,34 +1,70 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
+  Image,
   ListView,
+  StatusBar,
   StyleSheet,
   View,
 } from 'react-native';
 
+import listBackground from '../images/list-background.png';
 import Mini from './Mini';
 import MiniListLoading from './MiniListLoading';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  backgroundImage: {
+    height: '100%',
+    resizeMode: 'repeat',
+    position: 'absolute',
+    width: '100%',
   },
-  separator: {
+  container: {
+    backgroundColor: 'cyan',
+    flex: 1,
+    position: 'relative',
+  },
+  list: {
+  },
+  separator_1: {
+    backgroundColor: 'thistle',
+    height: 2,
+  },
+  separator_2: {
+    backgroundColor: 'palegoldenrod',
+    height: 2,
+  },
+  separator_3: {
+    backgroundColor: 'lightsteelblue',
+    height: 2,
   },
   separatorHighlighted: {
+    backgroundColor: 'steelblue',
+    height: 2,
+  },
+  top: {
+    backgroundColor: 'cyan',
+    height: 19,
+    shadowColor: 'cyan',
+    shadowOffset: {
+      height: 6,
+      width: 0,
+    },
+    shadowOpacity: 1,
+    zIndex: 100,
   },
 });
 
 export default class MiniList extends Component {
   static renderSeparator(sectionId, rowId, adjacentRowHighlighted) {
+    const style = adjacentRowHighlighted ?
+      styles.separatorHighlighted :
+      styles[`separator_${(rowId % 3) + 1}`];
+
     return (
       <View
         key={`${sectionId}-${rowId}`}
-        style={
-          adjacentRowHighlighted ?
-          styles.separatorHighlighted :
-          styles.separator
-        }
+        style={style}
       />
     );
   }
@@ -77,14 +113,25 @@ export default class MiniList extends Component {
 
   render() {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        onEndReached={this.props.onEndReached}
-        renderFooter={this.renderFooter}
-        renderRow={this.renderRow}
-        renderSeparator={MiniList.renderSeparator}
-        style={styles.container}
-      />
+      <View style={styles.container}>
+        <StatusBar
+          background="cyan"
+          barStyle="dark-content"
+        />
+        <Image
+          source={listBackground}
+          style={styles.backgroundImage}
+        />
+        <View style={styles.top} />
+        <ListView
+          dataSource={this.state.dataSource}
+          onEndReached={this.props.onEndReached}
+          renderFooter={this.renderFooter}
+          renderRow={this.renderRow}
+          renderSeparator={MiniList.renderSeparator}
+          style={styles.list}
+        />
+      </View>
     );
   }
 }
